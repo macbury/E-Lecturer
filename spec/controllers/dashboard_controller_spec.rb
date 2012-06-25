db6a86e15c91a66d1752f2db3c562670ebfc0f76
macbury <macbury@gmail.com>
2012-06-25 17:12:40 +0200

    dashboard spec

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -1,5 +1,32 @@
 require 'spec_helper'
 
 describe DashboardController do
-  #before { as_normal_user }
+  
+  context "as guest" do
+    before { as_guest! }
+    it "should redirect to login path for index action" do
+      get :index
+      response.should be_redirect
+      response.should redirect_to(new_user_session_path)
+    end
+  end
+
+  context "as student" do
+    before { as_student! }
+    it "should render index action" do
+      get :index
+      response.should render_tempalte("index")
+      response.should be_success
+    end
+  end
+
+  context "as lecturer" do
+    before { as_lecturer! }
+    it "should redirect root path" do
+      get :index
+      response.should be_redirect
+      response.should redirect_to(root_path)
+    end
+  end
+
 end
